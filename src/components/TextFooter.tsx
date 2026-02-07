689555dea2e13f13b0ad3799d1d78fbb20e30fcf
Mehdi Parlar <98858178+mehdiprl@users.noreply.github.com>
2026-02-07 22:16:55 +0100

    Edit TextFooter

--- src/components/TextFooter.tsx
+++ src/components/TextFooter.tsx
@@ -13,21 +13,15 @@ export default function TextFooter() {
       <h1
         className={`absolute left-10 bottom-5 transform -translate-y-1/2 text-white text-4xl lg:text-5xl font-bold leading-tight ${playfairDisplay.className}`}
       >
-        <span className="text-gray-400">Match</span> <br /> the photo pairs
+        <span className="text-gray-400">Von Mehdi</span> <br /> Für meine Aleyna
       </h1>
 
       {/* Right Text */}
       <h1
         className={`absolute right-10 bottom-5 transform -translate-y-1/2 text-white text-4xl lg:text-5xl font-bold leading-tight text-right ${playfairDisplay.className}`}
       >
-        to reveal <br /> <span className="text-gray-400">the surprise</span>
+        Spiele memory <br /> <span className="text-gray-400">Für die Überraschung</span>
       </h1>
-
-      {/* Copyright */}
-      <p className="absolute bottom-5 right-5 text-white text-sm opacity-15 hover:opacity-50">
-        © {new Date().getFullYear()}{" "}
-        <Link href="https://visibait.com">visibait.com</Link>
-      </p>
     </>
   );
 }
